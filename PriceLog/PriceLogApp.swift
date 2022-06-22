541342fa95a9d70724a2ef024b435e8a9d37fcd3
William Santoso <william.santoso@ruangguru.com>
2022-06-22 22:44:27 +0700

    Initial Commit

---
+++ PriceLog/PriceLogApp.swift
@@ -0,0 +1,20 @@
+//
+//  PriceLogApp.swift
+//  PriceLog
+//
+//  Created by William Santoso on 22/06/22.
+//
+
+import SwiftUI
+
+@main
+struct PriceLogApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
